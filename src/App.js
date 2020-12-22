397a37b162fed467b6dd1d7d5df6aa5572aeb51c
Yarden Lawson <yardenlawson@gmail.com>
2020-12-22 18:17:31 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-function App() {
+
+// import Home from './Home'
+// import Portfolio from './Portfolio'
+// import About from './About'
+// import Contact from './Contact'
+
+
+// import Header from './common/Header'
+// import Navbar from './common/Navbar'
+// import Footer from './common/Footer'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>hey</h1>
+    // <BrowserRouter>
+    // <Header />
+    // <Navbar />
+    //   <Switch>
+    //     <Route exact path="/" component={Home}/>
+    //     <Route path="/Portfolio" component={Portfolio}/>
+    //     <Route path="/About" component={About}/>
+    //     <Route path="/Contact" component={Contact}/>
+    //   </Switch>
+    //   <Footer />
+    // </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
