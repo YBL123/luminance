86b8506576c7d88e47768aee6cb879986e0fdf5c
Yarden Lawson <yardenlawson@gmail.com>
2020-12-23 13:13:33 +0000

    update

--- src/common/Navbar.js
+++ src/common/Navbar.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
 
+import navLogo from '../assets/Luminance_logos.png'
+
 class Navbar extends React.Component {
 
   render() {
-    
+
     return (
       <nav className="navbar">
-        <div className="navbar wrap">
-        <Link to="/" className="navbar-item">Home</Link> 
-        <Link to="/Products" className="navbar-item">Products</Link> 
-        <Link to="/Events" className="navbar-item">Events</Link> 
+        <div className="main-nav-wrap">
+          <div className="nav-logo-wrap">
+            <img className="nav-logo" src={navLogo} alt="nav-logo" />
+          </div>
+          <div className="navbar-wrap">
+            <Link to="/" className="navbar-item">Home</Link>
+            <Link to="/Products" className="navbar-item">Products</Link>
+            <Link to="/Events" className="navbar-item">Events</Link>
+          </div>
         </div>
       </nav >
     )
