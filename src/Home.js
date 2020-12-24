97792f2c63e63c2a324eace95c482c98a6943d6f
Yarden Lawson <yardenlawson@gmail.com>
2020-12-24 15:41:15 +0000

    update

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,46 @@ import React from 'react'
 const Home = () => {
 
   return (
-    <section className="home-header-section">
-      <div>
-        <h1>The artificial intelligence platform for the legal profession</h1>
-        <h3>
-          Analyse documents with unmatched speed and enhanced confidence, and apply that learning across all departments and practice areas
+    <>
+      <section className="home-header-section">
+        <div>
+          <h1>The artificial intelligence platform for the legal profession</h1>
+          <h3>
+            Analyse documents with unmatched speed and enhanced confidence, and apply that learning across all departments and practice areas
         </h3>
-      </div>
-    </section>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h1>
+            Award-winning AI technology
+          </h1>
+          <h2>
+            for the legal profession
+            </h2>
+          <h4>
+            Founded by mathematicians from the University of Cambridge, Luminance’s Legal Inference Transformation Engine (LITE) uniquely combines supervised and unsupervised machine learning to provide the most robust, powerful platform for legal analysis available to lawyers. Luminance’s technology can read and form an understanding of legal documentation in any language and jurisdiction, immediately surfacing the most relevant information and vastly reducing the amount of time spent on document review.
+            </h4>
+          <h5>
+            Luminance is used by over 250 law firms and organisations in over 50 countries and in more than 80 languages across a wide range of practice areas, including M&A due diligence, property portfolio analysis, eDiscovery, contract negotiation and model document comparison.
+            </h5>
+        </div>
+        <div>
+          <button>LEARN ABOUT DILIGIENCE</button>
+        </div>
+        <div>
+          <h4>250+
+          CUSTOMERS
+          50+
+          COUNTRIES
+          80+
+          LANGUAGES
+          IN USE BY OVER
+          ONE-FIFTH
+          OF GLOBAL TOP LAW FIRMS</h4>
+        </div>
+      </section>
+    </>
 
   )
 
