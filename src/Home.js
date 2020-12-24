ac9ce33456f6fe1b709392d8c9b4ae690386821c
Yarden Lawson <yardenlawson@gmail.com>
2020-12-24 14:26:19 +0000

    update

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,15 @@ import React from 'react'
 const Home = () => {
 
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="home-header-section">
+      <div>
+        <h1>The artificial intelligence platform for the legal profession</h1>
+        <h3>
+          Analyse documents with unmatched speed and enhanced confidence, and apply that learning across all departments and practice areas
+        </h3>
+      </div>
+    </section>
+
   )
 
 }
