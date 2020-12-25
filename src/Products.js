d2bb3c3e173526c6e84ac2bc9e9f88462a803709
Yarden Lawson <yardenlawson@gmail.com>
2020-12-25 14:43:50 +0000

    update

--- src/Products.js
+++ src/Products.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
+// import diligenceHeaderImage from './assets/diligence_header_image.jpg'
+import diligenceHeaderImage from './assets/Luminance-platform2.jpg'
+import luminanceDiligenceHeader from './assets/Luminance_diligence.png'
+
 const Products = () => {
 
   return (
-    <div>
-      <h1>Product</h1>
-    </div>
+    <section>
+      <div className="products-header-section-1-wrap">
+        <div className="prdoucts-diligience-header-image-wrap">
+        <img className="products-diligience-header-image" src={diligenceHeaderImage} alt="diligience-header" />
+        </div>
+        <div className="products-diligience-header-wrap">
+        <img className="products-diligience-header" src={luminanceDiligenceHeader} alt="luminance-diligience-header" />
+        </div>
+      </div>
+
+    </section>
   )
 
 }
