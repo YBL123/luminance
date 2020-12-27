e76508fb8179815b84b3437cdd6cb37aa28af01a
Yarden Lawson <yardenlawson@gmail.com>
2020-12-27 15:14:10 +0000

    update

--- src/Home.js
+++ src/Home.js
@@ -11,9 +11,9 @@ const Home = () => {
   return (
     <>
       <section className="home-header-section">
-        <div>
-          <h1>The artificial intelligence platform for the legal profession</h1>
-          <h3>
+        <div className="home-text-section-1">
+          <h1 className="home-text-section-1-h1">The artificial intelligence platform for the legal profession</h1>
+          <h3 className="home-text-section-1-h3">
             Analyse documents with unmatched speed and enhanced confidence, and apply that learning across all departments and practice areas
         </h3>
         </div>
